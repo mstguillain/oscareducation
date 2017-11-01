2882aa28f83cbd6e92a3909bd58e924814364cb0
Jonathan Samelson <jonathan@samelson.info>
2017-11-01 22:23:25 +0100

    Settings page working with post request

--- student_collaboration/views.py
+++ student_collaboration/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 from django.contrib.auth.decorators import login_required
 from django.forms import inlineformset_factory
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from student_collaboration.models import StudentCollaborator, CollaborativeSettings
@@ -11,32 +11,26 @@ from .forms import StudentCollaboratorForm, CollaborativeSettingsForm
 
 # Create your views here.
 @login_required
-def update_settings(request, collaborative_settings_pk=None):
+def update_settings(request):
     # requête de type POST; on update
-    if collaborative_settings_pk:
-        collaborative_settings = CollaborativeSettings.objects.get(pk=collaborative_settings_pk)
-    else:
-        collaborative_settings = CollaborativeSettings()
-
-    # if student_collaborator_pk:
-    #     student_collaborator = StudentCollaborator.objects.get(pk=student_collaborator_pk)
-    # else:
-    #     student_collaborator = StudentCollaborator()
+    student = get_object_or_404(StudentCollaborator, pk=request.user.studentcollaborator.pk)
+    settings = get_object_or_404(CollaborativeSettings, pk=request.user.studentcollaborator.settings.pk)
 
     if request.method == 'POST':
-        settings_set = CollaborativeSettingsForm(request.POST, instance=collaborative_settings)
-        student_form = StudentCollaboratorForm(request.POST, instance=collaborative_settings)
+        settings_form = CollaborativeSettingsForm(request.POST, instance=settings)
+        student_form = StudentCollaboratorForm(request.POST, instance=student)
 
-        if student_form.is_valid() and settings_set.is_valid():
-            settings_set.save()
-            student_form.save()
-            return HttpResponse(status=200)
-        else:
-            return HttpResponse(status=400)
-    else:
-        collaborative_form = CollaborativeSettingsForm(instance=collaborative_settings)
-        student_form = StudentCollaboratorForm(instance=collaborative_settings)
+        if student_form.is_valid() and settings_form.is_valid():
+            settings = settings_form.save(commit=False)
+            student = student_form.save(commit=False)
+            student_form.settings = settings
+            settings.save()
+            student.save()
+
+    # else:
+    settings_form = CollaborativeSettingsForm(instance=settings)
+    student_form = StudentCollaboratorForm(instance=student)
     return render(request, 'student_collaboration/student_settings.haml', {
         'student_form': student_form,
-        'collaborative_form': collaborative_form
+        'settings_form': settings_form
     })
