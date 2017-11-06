c3088f4e729937542f1c6dec5190567ff986d994
PKriwin <39171@heb.be>
2017-11-06 17:17:02 +0100

    fix: skills wrongly filled on error (#34)

--- forum/views.py
+++ forum/views.py
@@ -17,13 +17,6 @@ from users.models import Professor
 
 from dashboard import get_thread_set
 
-
-class ThreadForm(forms.Form):
-    title = forms.TextInput()
-    content = forms.Textarea()
-    visibility = forms.ChoiceField()
-
-
 class MessageReplyForm(forms.ModelForm):
     class Meta:
         model = Message
@@ -52,16 +45,16 @@ def create_thread(request):
 
 
 def get_create_thread_page(request):
-    return render(request, "forum/new_thread.haml", {'errors': [], "data": {
-        'title': "",
-        'visibility': "private",
-        'visibdata': "",
-        'skills': "",
-        'content': ""
-    }})
-
+    return render(request, "forum/new_thread.haml", { 'errors' : [], "data": {
+            'title' : "",
+            'visibility': "private",
+            'visibdata' : "",
+            'skills' : "",
+            'content' : ""
+        } })
 
 def post_create_thread(request):
+
     errors = []
     params = deepValidateAndFetch(request, errors)
 
@@ -81,8 +74,10 @@ def post_create_thread(request):
                 thread.professor = params['professor']
 
             thread.save()
-            thread.skills = params['skills']
-            thread.save()
+
+            if params['skills_fetched']:
+                thread.skills = params['fetched_skills']
+                thread.save()
 
             original_message = Message(content=params['content'], thread=thread, author=params['author'])
             original_message.save()
@@ -90,8 +85,7 @@ def post_create_thread(request):
         return redirect('/forum/thread/' + str(thread.id))
 
     else:
-        return render(request, "forum/new_thread.haml", {"errors": errors, "data": params})
-
+        return render(request, "forum/new_thread.haml", { "errors" : errors, "data": params })
 
 class ThreadForm(forms.Form):
     title = forms.CharField()
@@ -99,14 +93,15 @@ class ThreadForm(forms.Form):
     skills = forms.CharField()
     content = forms.CharField()
 
-
 def deepValidateAndFetch(request, errors):
+
     params = {}
     form = ThreadForm(request.POST)
 
     form.is_valid()
 
     params['visibility'] = request.POST.get('visibility')
+    params['skills_fetched'] = False
 
     try:
         params['skills'] = form.cleaned_data['skills']
@@ -117,54 +112,53 @@ def deepValidateAndFetch(request, errors):
         params['title'] = form.cleaned_data['title']
     except:
         params['title'] = ""
-        errors.append({"field": "title", "msg": "Le titre du sujet ne peut pas être vide"})
+        errors.append({ "field": "title", "msg" :"Le titre du sujet ne peut pas être vide"})
 
     try:
         params['visibdata'] = form.cleaned_data['visibdata']
     except:
         params['visibdata'] = ""
-        errors.append({"field": "visibdata", "msg": "Le paramètre de visibilité ne peut pas être vide"})
+        errors.append({ "field": "visibdata", "msg" :"Ce champs ne peut pas être vide"})
 
     try:
         params['content'] = form.cleaned_data['content']
     except:
         params['content'] = ""
-        errors.append({"field": "content", "msg": "Le premier message du sujet ne peut pas être vide"})
+        errors.append({ "field": "content", "msg" :"Le premier message du sujet ne peut pas être vide"})
 
     try:
         params['author'] = User.objects.get(pk=request.user.id)
     except:
-        errors.append({"field": "visibdata", "msg": "Auteur inconnu"})
+        errors.append({ "field": "visibdata", "msg" :"Auteur inconnu"})
 
     if params['visibility'] not in ["private", "class", "public"]:
-        errors.append({"field": "visibility", "msg": "Type de visibilité invalide"})
+        errors.append({ "field": "visibility", "msg" :"Type de visibilité invalide"})
 
     if params['visibdata'] != "":
-
         if params['visibility'] == "private":
             try:
                 params['recipient'] = User.objects.get(pk=params['visibdata'])
             except:
-                errors.append({"field": "visibdata", "msg": "Destinataire inconnu"})
+                errors.append({ "field": "visibdata", "msg" :"Destinataire inconnu"})
 
         if params['visibility'] == "class":
             try:
                 params['lesson'] = Lesson.objects.get(pk=params['visibdata'])
             except:
-                errors.append({"field": "visibdata", "msg": "Classe inconnue"})
+                errors.append({ "field": "visibdata", "msg" :"Classe inconnue"})
 
         if params['visibility'] == "public":
             try:
-                params['professor'] = Professor.objects.get(pk=params['visibdata'])
+                params['professor'] =  Professor.objects.get(pk=params['visibdata'])
             except:
-                errors.append({"field": "visibdata", "msg": "Professeur inconnu"})
+                errors.append({ "field": "visibdata", "msg" :"Professeur inconnu"})
 
     if params['skills'] != "":
         try:
-            params['skills'] = Skill.objects.filter(pk__in=params['skills'].encode('utf8').split(" "))
+            params['fetched_skills'] = Skill.objects.filter(pk__in=params['skills'].encode('utf8').split(" "))
+            params['skills_fetched'] = True
         except:
-            errors.append(
-                {"field": "skills", "msg": "Compétence(s) inconnue(s) ou mal formée(s) (format: id1 id2 ...)"})
+            errors.append({ "field": "skills", "msg" :"Compétence(s) inconnue(s) ou mal formée(s) (format: id1 id2 ...)"})
 
     return params
 
