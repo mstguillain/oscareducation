a8f9aff332423d6a849a311da189f6c874a694f7
Loan181 <loan181@hotmail.fr>
2017-11-24 19:01:37 +0100

    add new functionalities & refactor 2 students can interact with each other

--- student_collaboration/selenium/selen.py
+++ student_collaboration/selenium/selen.py
@@ -10,8 +10,10 @@ from selenium.webdriver.common.action_chains import ActionChains
 import re
 
 BASE_URL = "http://127.0.0.1:8000/"
+
 ADMIN_PSEUDO = "root"
 ADMIN_PASSWORD = "root"
+
 TEACHER_PSEUDO = "TestTeacher"
 TEACHER_PASSWORD = "test"
 CLASS_NAME = "testClass"
@@ -25,11 +27,24 @@ STUDENTS_SKILLS = (
 	{"unmastered": ("P3D-U3-T2", "P3D-U3-C2"), "mastered": ("P3D-U3-A2",)}
 )
 
+STUDENT_ID = (
+	1146,
+	1147
+)
+
+STUDENTS_PASSWORD = "test"
+
+STUDENTS_COLLABORATIVE_TOOLS_PARAMS = (
+	(1180, 6),
+	(1180, 7)
+)
+
 
 class Selenium:
 	TIMEOUT_BUTTON_WAIT = 15
 	def __init__(self):
 		self.__initDriver()
+		self.__hideDjangoDebug()
 
 	def __initDriver(self):
 		""" See readme file for more explication if trouble to install """
@@ -39,26 +54,75 @@ class Selenium:
 			self.driver = webdriver.Chrome()
 		self.driver.wait = WebDriverWait(self.driver, 1)
 
+	def __hideDjangoDebug(self):
+		""" Django DEbug Tab sometime overlap buttons that selenium try to click, so we hide it"""
+		self.driver.get(BASE_URL)
+		hideButton = self.__waitElementById("djHideToolBarButton")
+		hideButton.click()
 
-	def test(self):
+
+	def testAll(self):
 		""" All tests """
-		#self.__testFunction(self.logInAdmin, ADMIN_PSEUDO, ADMIN_PASSWORD)
-		#self.__testFunction(self.logOutAdmin)
+		#self.testAdmin(ADMIN_PSEUDO, ADMIN_PASSWORD)
 
-		self.__testFunction(self.logInTeacher, TEACHER_PSEUDO, TEACHER_PASSWORD)
-		self.__testFunction(self.addClass, CLASS_NAME)
-		self.__testFunction(self.addStudents, STUDENTS)
-		self.__testFunction(self.setSkillsToStudent, STUDENTS_SKILLS)
+		#self.testTeacherClassCreation(TEACHER_PSEUDO, TEACHER_PASSWORD, CLASS_NAME, STUDENTS, STUDENTS_SKILLS)
+
+		#studentsPseudo = self.testRegisterStudents()
+
+		studentsPseudo = ["fnselen11.lnselen116", "fnselen22.lnselen226"] # TODO : delete
+		self.testCollaborativeTool(studentsPseudo, STUDENTS_COLLABORATIVE_TOOLS_PARAMS)
+
+
 
 		#self.fakeTest()
+
+	def testAdmin(self, admin_pseudo, admin_password):
+		self.__testFunction(self.logInAdmin, admin_pseudo, admin_password)
+		self.__testFunction(self.logOutAdmin)
+
+	def testTeacherClassCreation(self, teacher_pseudo, teacher_password, class_name, students, students_skills):
+		self.__testFunction(self.logInTeacher, teacher_pseudo, teacher_password)
+		self.__testFunction(self.addClass, class_name)
+		self.__testFunction(self.addStudents, students)
+		self.__testFunction(self.setSkillsToStudent, students_skills)
+
+	def testRegisterStudents(self):
+		studentCodes = self.__testFunction(self.getStudentsCode)
+		self.logOutUser()
+		for studentUserName, studentCode in studentCodes.iteritems():
+			self.__testFunction(self.logInStudent, studentUserName, STUDENTS_PASSWORD, studentCode)
+			self.logOutUser()
+		return studentCodes.keys()
+
+	def testCollaborativeTool(self, studentsPseudo, students_collaborative_tools_params):
+		# With the first user we create a help request
+		curIndex = 0 # First student index
+		self.__testFunction(self.logInStudent, studentsPseudo[curIndex], STUDENTS_PASSWORD)
+		studentParams = students_collaborative_tools_params[curIndex]
+		self.__testFunction(self.activateCollaborativeTool, studentParams[0], studentParams[1])
+		# The first to connect ask for help
+		askRequestText = self.__testFunction(self.askHelp)
+		print 'ask'+askRequestText
+		self.logOutUser()
+
+		# With the second one we check that the help request exists
+		curIndex = 1 # We move to the second student
+		self.__testFunction(self.logInStudent, studentsPseudo[curIndex], STUDENTS_PASSWORD)
+		studentParams = students_collaborative_tools_params[curIndex]
+		self.__testFunction(self.activateCollaborativeTool, studentParams[0], studentParams[1])
+		self.__testFunction(self.acceptHelpRequest)
+		self.logOutUser()
+
+		# TODO : créer la discussion etc etc.
 
 	def __testFunction(self, fun, *args):
 		""" Test a given function on the given arguements"""
 		functionName = fun.im_func.__name__
 		print("Testing "+functionName+" with "+str(args)+" : ")
 		try:
-			fun(*args)
+			ret = fun(*args)
 			print("\tOK")
+			return ret
 		except TimeoutException as e:
 			traceback.print_exc(e)
 			print("\tERROR (Timeout) : "+ str(e.args) +" One of the demanded ressource took to much time to be found. It's likely that it didn't existed or the page wasn't loaded/redirected as expected")
@@ -83,6 +147,12 @@ class Selenium:
 	def __waitElementByClassName(self, className):
 		return self.driver.wait.until(EC.presence_of_element_located((By.CLASS_NAME, className)))
 
+	def __waitElementByCSSSlector(self, css):
+		return self.driver.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, css)))
+
+	def __waitElementById(self, id):
+		return self.driver.wait.until(EC.presence_of_element_located((By.ID, id)))
+
 	def __fillBoxSubmit(self, name, text):
 		box = self.__fillBox(name, text)
 		box.submit()
@@ -91,6 +161,9 @@ class Selenium:
 		box = self.__waitElementByName(name)
 		box.send_keys(text)
 		return box
+
+	def __countRawsInTable(self, xPathTable):
+		return len(self.driver.find_elements_by_xpath(xPathTable+"/tbody/tr"))
 
 	def __clickButtonByXPath(self, xPath):
 		button = WebDriverWait(self.driver, self.TIMEOUT_BUTTON_WAIT).until(EC.element_to_be_clickable((By.XPATH, xPath)))
@@ -123,10 +196,9 @@ class Selenium:
 
 	def logInTeacher(self, teacherPseudo, teacherPassword):
 		""" We consider that the teacher already exists """
-		self.driver.get(BASE_URL+"accounts/usernamelogin")
-		# First enter the log in
-		self.__fillBoxSubmit("username", teacherPseudo)
-		self.__testURL("accounts/passwordlogin/", r"The username passwords didn't show up", r"Are you sure that this username aws created ?")
+		self.__logInFirstStep(teacherPseudo)
+		self.__testURL("accounts/passwordlogin/", r"The username passwords didn't show up",
+		               r"Are you sure that this username aws created ?")
 		# As a teacher the tag is "password" "code" for students
 		self.__fillBoxSubmit("password", teacherPassword)
 		self.__testURL("professor/dashboard/", r"Teacher have not be logged in sucessfully", "Are you sure that this teacher existed ?")
@@ -217,81 +289,131 @@ class Selenium:
 					child = test.find_element_by_xpath("//div[2]/center/form[" + str(skillIdx) + "]")
 					child.submit()
 
+	def getStudentsCode(self):
+		""" We consider that we are on a lesson page """
+		self.driver.get("http://127.0.0.1:8000/professor/lesson/7/")# TODO : remove
+		strDashbord = "professor/lesson/"
+		url = self.driver.current_url
+		professorDashbordURL = url[:url.find("/", url.find(strDashbord)+len(strDashbord))+1]
+		codePage = professorDashbordURL+"students_password_page/"
+		self.driver.get(codePage)
+		table = self.driver.find_elements_by_xpath("/html/body/table")
+		studentList = table[0].text.split()
+		studentDict = {}
+		for i in range(len(studentList) // 8):
+			studentDict[studentList[4 + i * 8].decode()] = studentList[7 + i * 8].decode()
+		return studentDict
+
+	def __logInFirstStep(self, userName):
+		self.driver.get(BASE_URL + "accounts/usernamelogin")
+		self.__testURL("accounts/usernamelogin", r"You can't access the log in page", r"Are you sure that no user is already logged in ?")
+		# First enter the log in
+		self.__fillBoxSubmit("username", userName)
+
+
+	def logInStudent(self, studentUserName, studentPassword, studentCode=None):
+		"""
+		Log in an existing student.
+		We consider that no user is logged in already
+
+		:param studentUserName: The student username
+		:type studentUserName: str
+		:param studentPassword: The stundent password, if he register for the first time, it will aso be the new password
+		:type studentPassword: str
+		:param studentCode: The student register code that the teacher know. It's mandatory if it is its  first registration
+		:type studentCode: str
+		"""
+		self.__logInFirstStep(studentUserName)
+		# If if is the code login page
+		urlEnd = "/codelogin/"
+		if self.driver.current_url.endswith(urlEnd):
+			# Add to password entries
+			self.__fillBoxSubmit("code", studentCode)
+			self.__testURL("accounts/createpassword/", r"The student could not register his password", r"Was the student code correct correct ?")
+
+			# New page asking password
+			self.__fillBox("password", studentPassword)
+			self.__fillBoxSubmit("confirmed_password", studentPassword)
+		else: # Here the classic login page
+			self.__fillBoxSubmit("password", studentPassword)
+		self.__testURL("student/dashboard/", r"The student could not logged in to his dashboard", r"Was the password correct ?")
+
+
+	def activateCollaborativeTool(self, postalCode, distance):
+		""" We assume that a student user is currently logged in """
+		self.driver.get(BASE_URL+"student_collaboration/settings/")
+		self.__testURL("student_collaboration/settings/", r"The collaborative tool can't be accessed", r"Are you sure that a user is logged in already and that it is a student ?")
+
+		# Enable the collaborative tool it is not already
+		collaborativeCheckBox = self.__waitElementById("collaborative_tool_check")
+		isAlreadyActivate = collaborativeCheckBox.is_selected()
+
+		if not isAlreadyActivate:
+			toggle = self.__waitElementByClassName("switch")
+			toggle.click()
+		else:
+			print "\tWarning : The collaborative tool was already activated. It's content have still been refreshed"
+		postalCodeBox = self.__waitElementByName("postal_code")
+		postalCodeBox.send_keys(postalCode)
+
+		distanceBox = self.__waitElementByName("distance")
+		distanceBox.clear()
+		distanceBox.send_keys(distance)
+
+		distanceBox.submit()
+
+	def askHelp(self):
+		""" Will automatically ask help for his first unmastered skill.
+		 We consider that the a student is logged in & have activated its collaborative tool & have at least one un mastered skill """
+		self.driver.get(BASE_URL+"student_collaboration/request_help/")
+		self.__testURL("student_collaboration/request_help/", r"The collaborative tool asking help page wasn't loaded properly", r"Are you sure that a student is logged in and already activate its contributive tool ?")
+
+		# Make the dropdown appear
+		dropdown = self.__waitElementByCSSSlector(".multiselect.dropdown-toggle.btn.btn-default")
+		dropdown.click()
+		# Check the checkbox of the first unmastered skill (to hard to find the corresponding skill)
+		xPathFirstCheck = "/html/body/div[2]/div[3]/div/form/div/div/span/div/ul/li[1]/a/label/input"
+		firstUnmasteredSkill = self.__waitElementByXPath(xPathFirstCheck)
+		skillText = self.__waitElementByXPath("/html/body/div[2]/div[3]/div/form/div/div/span/div/ul/li[1]/a/label").text
+		firstUnmasteredSkill.click()
+		firstUnmasteredSkill.submit()
+#		self.__waitElementById("send_help_request").click()
+		# After submission we are redircted to the history tab
+		try:
+			self.__testURL("student_collaboration/help_request_history/", r"The help request history page was not shawn")
+		except BaseException as e:
+			print '\tWarning : The redirection to history failed, perhaps this help request already exists?'
+		return skillText
+
+	def acceptHelpRequest(self):
+		""" Will automatically accept help requests
+		We consider that the a student is logged in & have activated its collaborative tool & their is at least one, the first one will be accepted"""
+		self.driver.get(BASE_URL+"student_collaboration/provide_help/")
+		self.__testURL("student_collaboration/provide_help/", r"The collaborative tool asking help page wasn't loaded properly", r"Are you sure that a student is logged in and already activate its contributive tool ?")
+		# Find the length of the table
+		xPathTable = "//table[@id='provide-help']"
+		sizeBeforeAcceptation = self.__countRawsInTable(xPathTable)
+
+
+		# Find the accept button
+		xPathTableText = '//*[@id="provide-help"]/tbody[2]/tr/td[2]/li'
+		skillText = self.__waitElementByXPath(xPathTableText).text
+		xPathFirstButton = '//*[@id="provide-help"]/tbody[2]/tr[1]/td[4]/button'
+		# WARNING : it layout have not changed, the Accept button will probably be overlapped by the django debug tab
+		# That's why we hide it on the object constructor
+		self.__clickButtonByXPath(xPathFirstButton)
+
+		if self.__countRawsInTable(xPathTable) != sizeBeforeAcceptation-1:
+			raise BaseException(r"Table sized didn't decrement after request approval")
+		return skillText
+
+
 	def fakeTest(self):
 		""" ca c'est juste pour faire des petit test vite fait """
 		url = "http://127.0.0.1:8000/professor/lesson/4/student/1140/#heatmap"
 		self.driver.get(url)
 
-##### NEED TO BE ADAPTED ######
-# students_password_page
-def getStudentsPassword():
-	driver.get(driver.current_url + "students_password_page")
-	table = driver.find_elements_by_xpath("/html/body/table")
-	studentList = table[0].text.split()
-	studentDict = {}
-	for i in range(len(studentList) // 8):
-		studentDict[studentList[1 + i * 8].decode() + " " + studentList[i * 8].decode()] = [
-			studentList[4 + i * 8].decode(), studentList[7 + i * 8].decode()]
-	return studentDict
-
-
-def addPassword(pwd):
-	addValByName(pwd, "password")
-	addValByName(pwd, "confirmed_password", submit=True)
 
 if __name__ == '__main__':
 	selenium = Selenium()
-	selenium.test()
-
-
-if False: # Ancien main, on l'apelle pas
-	admin = "root"
-	adminPwd = "root"
-	#classId = 29
-	skillCode = "P3D-U3-T2"
-	studentList = [["fnselen11", "lnselen11"], ["fnselen22", "lnselen22"]]
-	driver = init_driver()
-	# loginAdmin(driver, admin, adminPwd)
-	# logoutAdmin(driver)
-	login("prof02", "prof02", student=False)
-	# profAddClass("selenium03")
-	profGetClass("selenium03")
-	stPwdDict = getStudentsPassword()
-	# print(stPwdDict)
-	# profGetClass("selenium03")
-	# addSkillToStudent(stPwdDict.keys()[0], skillCode)
-	# time.sleep(8)
-
-	# profGetClass("selenium03")
-	# addSkillToStudent(stPwdDict.keys()[1], skillCode)
-	# time.sleep(8)
-
-
-
-	# profAddStudents(studentList)
-	# profSetSkill(classId, skillCode)
-
-	logout()
-	for student in stPwdDict.keys():
-		print(stPwdDict[student][0] + " " + str(stPwdDict[student][1]))
-		login(stPwdDict[student][0], str(stPwdDict[student][1]))
-		if "http://localhost:8000/accounts/createpassword/" == driver.current_url:
-			addPassword(student)
-		driver.get("http://localhost:8000/student_collaboration/settings/")
-
-		# addValByName(20, "distance")
-
-		optionSelector = driver.wait.until(EC.presence_of_element_located((By.NAME, "option")))
-		# for option in optionSelector.find_elements_by_name('1100'):
-		#    if option.text == "1100":
-		#        option.click()
-
-		# collabTool = driver.wait.until(EC.presence_of_element_located((By.NAME, "collaborative_tool")))
-		# collabTool.submit()
-
-		time.sleep(300)
-		logout()
-
-		# logout(driver)
-		# driver.close()
-		# driver.quit()
+	selenium.testAll()
