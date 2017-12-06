bc4514dab85c8312e589b9cb019b368d9397a14b
Lionel Ovaert <lionel.ovaert@live.be>
2017-12-06 13:01:04 +0100

    Fixed notifications in home-page and implemented mark all as read

--- oscar/static/js/notify.js
+++ oscar/static/js/notify.js
@@ -1,12 +1,72 @@
-var notify_badge_class;
-var notify_menu_class;
+var notify_badge_class = 'notifications-badge-class';
+var notify_menu_class= 'notifications-list-class';//
 var notify_api_url;
 var notify_fetch_count;
 var notify_unread_url;
 var notify_mark_all_unread_url;
-var notify_refresh_period = 15000;
+var notify_refresh_period = 30000;
 var consecutive_misfires = 0;
 var registered_functions = [];
+moment.locale('fr', {
+    months : 'janvier_février_mars_avril_mai_juin_juillet_août_septembre_octobre_novembre_décembre'.split('_'),
+    monthsShort : 'janv._févr._mars_avr._mai_juin_juil._août_sept._oct._nov._déc.'.split('_'),
+    monthsParseExact : true,
+    weekdays : 'dimanche_lundi_mardi_mercredi_jeudi_vendredi_samedi'.split('_'),
+    weekdaysShort : 'dim._lun._mar._mer._jeu._ven._sam.'.split('_'),
+    weekdaysMin : 'Di_Lu_Ma_Me_Je_Ve_Sa'.split('_'),
+    weekdaysParseExact : true,
+    longDateFormat : {
+        LT : 'HH:mm',
+        LTS : 'HH:mm:ss',
+        L : 'DD/MM/YYYY',
+        LL : 'D MMMM YYYY',
+        LLL : 'D MMMM YYYY HH:mm',
+        LLLL : 'dddd D MMMM YYYY HH:mm'
+    },
+    calendar : {
+        sameDay : '[Aujourd’hui à] LT',
+        nextDay : '[Demain à] LT',
+        nextWeek : 'dddd [à] LT',
+        lastDay : '[Hier à] LT',
+        lastWeek : 'dddd [dernier à] LT',
+        sameElse : 'L'
+    },
+    relativeTime : {
+        future : 'dans %s',
+        past : 'il y a %s',
+        s : 'quelques secondes',
+        m : 'une minute',
+        mm : '%d minutes',
+        h : 'une heure',
+        hh : '%d heures',
+        d : 'un jour',
+        dd : '%d jours',
+        M : 'un mois',
+        MM : '%d mois',
+        y : 'un an',
+        yy : '%d ans'
+    },
+    dayOfMonthOrdinalParse : /\d{1,2}(er|e)/,
+    ordinal : function (number) {
+        return number + (number === 1 ? 'er' : 'e');
+    },
+    meridiemParse : /PD|MD/,
+    isPM : function (input) {
+        return input.charAt(0) === 'M';
+    },
+    // In case the meridiem units are not separated around 12, then implement
+    // this function (look at locale/id.js for an example).
+    // meridiemHour : function (hour, meridiem) {
+    //     return /* 0-23 hour, given meridiem token and hour 1-12 */ ;
+    // },
+    meridiem : function (hours, minutes, isLower) {
+        return hours < 12 ? 'PD' : 'MD';
+    },
+    week : {
+        dow : 1, // Monday is the first day of the week.
+        doy : 4  // The week that contains Jan 4th is the first week of the year.
+    }
+});
 
 function fill_notification_badge(data) {
     var badges = document.getElementsByClassName(notify_badge_class);
@@ -32,8 +92,9 @@ function fill_notification_list(data) {
                 message = message + " " + item.target;
             }
             if(typeof item.timestamp !== 'undefined'){
-                message = message + " " + item.timestamp;
+                message = message + " " + moment(item.timestamp).format('LLLL');//DD/MM/YYYY k:mm:s
             }
+            // message = message + " " + '<a href="/inbox/notifications/mark_as_read">' + 'Marquer comme lu' +'</a>';
             return '<li>' + message + '</li>';
         }).join('')
 
